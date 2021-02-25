7a325ee2e20897e72ee945f3cb61244cec11d461
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-25 14:25:26 -0500

    feat(service/items.js): fetching data from ML api

--- src/services/items.js
+++ src/services/items.js
@@ -1,21 +1,24 @@
-const itemsObject = [
-  {
-    id: 0,
-    name: 'Guitarra Jackson'
-  },
-  {
-    id: 1,
-    name: 'Guitarra Ibanez'
-  },
-  {
-    id: 2,
-    name: 'Guitarra Dean'
-  }
-]
+const fetch = require('node-fetch')
+
+// const itemsObject = [
+//   {
+//     id: 0,
+//     name: 'Guitarra Jackson'
+//   },
+//   {
+//     id: 1,
+//     name: 'Guitarra Ibanez'
+//   },
+//   {
+//     id: 2,
+//     name: 'Guitarra Dean'
+//   }
+// ]
 
 class ItemsService {
   async getItems () {
-    const items = await Promise.resolve(itemsObject)
+    const response = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=guitarra')
+    const items = response.json()
     return items || []
   }
 }
