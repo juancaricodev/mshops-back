b2096edfc25511d494810b60a57ca911b9727d97
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-25 23:40:52 -0500

    feat(itemId): fetching data from service

---
+++ src/services/itemId.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch')
+
+class ItemIdService {
+  async getItemId (itemId) {
+    const response = await fetch(`https://api.mercadolibre.com/items/${itemId}`)
+    const item = await response.json()
+
+    return item
+  }
+}
+
+module.exports = ItemIdService
