a617cdc4b20d6d81bf06d03951e43c3158b86ded
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-02 11:24:35 -0500

    feat(utils/formatItems): formatItems function created; utils folder created

--- src/services/items.js
+++ src/services/items.js
@@ -1,25 +1,29 @@
 const fetch = require('node-fetch')
 
+const formatItems = require('../utils/formatItems')
+
 class ItemsService {
   async getItems (query) {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
     const items = await response.json()
 
-    const Items = items.results.map((item) => {
-      return {
-        id: item.id,
-        title: item.title,
-        price: {
-          currency: item.currency_id,
-          amount: Math.trunc(item.price),
-          decimals: item.price - Math.trunc(item.price)
-        },
-        picture: item.thumbnail,
-        condition: item.condition,
-        free_shipping: item.shipping.free_shipping,
-        address: item.address.state_name
-      }
-    })
+    // const Items = items.results.map((item) => {
+    //   return {
+    //     id: item.id,
+    //     title: item.title,
+    //     price: {
+    //       currency: item.currency_id,
+    //       amount: Math.trunc(item.price),
+    //       decimals: item.price - Math.trunc(item.price)
+    //     },
+    //     picture: item.thumbnail,
+    //     condition: item.condition,
+    //     free_shipping: item.shipping.free_shipping,
+    //     address: item.address.state_name
+    //   }
+    // })
+
+    const Items = formatItems(items.results)
 
     const Categories = items.filters.filter(e => e.id === 'category')[0].values[0].path_from_root.map(e => e.name)
 
